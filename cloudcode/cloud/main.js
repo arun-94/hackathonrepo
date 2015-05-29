17a0e67387fe9f1be47ffbab738c9a7fb742e8e1
arun-94 <arunswaminathan94@gmail.com>
2015-05-30 01:34:25 +0530

    Added functions

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -1,29 +1,85 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function (request, response) {
+Parse.Cloud.define("MiniStatement", function (request, response) {
 
-	var Buffer = require('buffer').Buffer,
-		buffer = new Buffer(
-			'<?xml version="1.0" encoding="utf-8"?>' +
-			'<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">' +
-			'   <soap12:Body>' +
-			'       <get_server_time xmlns="http://tempuri.org/" />' +
-			'   </soap12:Body>' +
-			'</soap12:Envelope>'
-		);
+	var soap = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org"> <soapenv:Header/> <soapenv:Body> <tem:MiniStatement_Request><tem:Header><tem:UniqueId>MINIST001</tem:UniqueId><tem:ServiceRequestId>MW.HKN</tem:ServiceRequestId><tem:ServiceRequestVersion>1.0</tem:ServiceRequestVersion> <tem:ChannelId>HKN</tem:ChannelId> </tem:Header> <tem:MiniStatement_Req_Body> <tem:RequestDate>2015-05-18</tem:RequestDate> <tem:Currency>INR</tem:Currency> <tem:AccountInfo> <tem:AccountId>ACT000000000001</tem:AccountId> </tem:AccountInfo> </tem:MiniStatement_Req_Body> </tem:MiniStatement_Request> </soapenv:Body> </soapenv:Envelope>';
 
 	Parse.Cloud.httpRequest({
 		method: 'POST',
-		url: 'https://services.rs.ge/WayBillService/WayBillService.asmx',
+		url: 'http://hackathon.axisbank.com:8523/CoreMWService',
 		headers: {
-			'Content-Type': 'text/xml; charset=utf-8'
+			'Content-Type': 'text/xml'
 		},
-		body: buffer,
+		body: soap,
 		success: function (httpResponse) {
-			response.success(httpResponse.text);
+			var xmlreader = require('cloud/xml-reader.js');
+			var temp = httpResponse.text;
+			temp = temp.replace(/:/g, '');
+			var summary = [];
+			xmlreader.read(temp, function (err, res) {
+				if (err) return console.log(err);
+				var abcd = res.NS1Envelope.NS1Body.NS2MiniStatement_Response.NS2MiniStatement_Rsp_Body;
+				console.log(abcd.NS2AccountBalance);
+				summary.push(abcd.NS2RequestDate.text());
+				summary.push(abcd.NS2Currency.text());
+				summary.push(abcd.NS2Status.text());
+				var accountBalance = abcd.NS2AccountBalance.array;
+				var temp1 = [];
+				for (var i = 0; i < accountBalance.length; i++) {
+					temp1.push(accountBalance[i].NS2BalanceType.text());
+					temp1.push(accountBalance[i].NS2BalanceAmount.NS2Amount.text());
+				}
+				summary.push(temp1);
+
+				var temp2 = [];
+				var trnDetails = abcd.NS2TrnDetail.NS2TransactionDetails.array;
+				for (var i = 0; i < trnDetails.length; i++) {
+					temp2.push(trnDetails[i].text());
+				}
+				summary.push(temp2);
+			});
+			response.success(summary);
 		},
 		error: function (httpResponse) {
-			response.error('Request failed with response code ' + httpResponse.status);
+			response.error(httpResponse.message);
+		}
+	});
+});
+
+Parse.Cloud.define("GetBalance", function (request, response) {
+
+	var soap = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org"> <soapenv:Header/> <soapenv:Body> <tem:GetBalance_Request> <tem:Header> <tem:UniqueId>000000008</tem:UniqueId> <tem:ServiceRequestId>MW.HKN</tem:ServiceRequestId> <tem:ServiceRequestVersion>1.0</tem:ServiceRequestVersion> <tem:ChannelId>HKN</tem:ChannelId> </tem:Header> <tem:GetBalance_Req_Body> <tem:RequestDate>2015-05-18</tem:RequestDate> <tem:Currency>INR</tem:Currency> <tem:AccountInfo> <tem:AccountId>ACT000000000001</tem:AccountId> </tem:AccountInfo> </tem:GetBalance_Req_Body> </tem:GetBalance_Request> </soapenv:Body> </soapenv:Envelope>';
+	Parse.Cloud.httpRequest({
+		method: 'POST',
+		url: 'http://hackathon.axisbank.com:8523/CoreMWService',
+		headers: {
+			'Content-Type': 'text/xml'
+		},
+		body: soap,
+		success: function (httpResponse) {
+			var xmlreader = require('cloud/xml-reader.js');
+			var temp = httpResponse.text;
+			temp = temp.replace(/:/g, '');
+			var summary = [];
+			xmlreader.read(temp, function (err, res) {
+				if (err) return console.log(err);
+				var abcd = res.NS1Envelope.NS1Body.NS2GetBalance_Response.NS2GetBalance_Rsp_Body;
+				console.log(abcd.NS2AccountBalance);
+				summary.push(abcd.NS2RequestDate.text());
+				summary.push(abcd.NS2Currency.text());
+				summary.push(abcd.NS2Status.text());
+				var accountBalance = abcd.NS2AccountBalance.array;
+				var temp1 = [];
+				for (var i = 0; i < accountBalance.length; i++) {
+					temp1.push(accountBalance[i].NS2BalanceType.text());
+					temp1.push(accountBalance[i].NS2BalanceAmount.NS2Amount.text());
+				}
+				summary.push(temp1);
+			});
+			response.success(summary);
+		},
+		error: function (httpResponse) {
+			response.error(httpResponse.message);
 		}
 	});
 });
--- js/functions.js
+++ js/functions.js
@@ -2,14 +2,24 @@ function init() {
 	Parse.initialize("5CacqV3GaCWICl8LV19LXZ2G2JbzPmjlupfDKIte", "F5kE5mcmOy2wKS8qfRcPrRICSKdh7f3tjU8V2Qod");
 	console.log("Parse Initialized");
 
-	cloudCall();
+	GetBalance();
 }
 
-function cloudCall() {
-	Parse.Cloud.run('hello', {}, {
+function MiniStatement() {
+	Parse.Cloud.run('MiniStatement', {}, {
 		success: function (result) {
 			console.log(result);
-			// result is 'Hello world!'
+		},
+		error: function (error) {
+			console.log(error);
+		}
+	});
+}
+
+function GetBalance() {
+	Parse.Cloud.run('GetBalance', {}, {
+		success: function (result) {
+			console.log(result);
 		},
 		error: function (error) {
 			console.log(error);
