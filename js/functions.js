34c53a92468ac6b8a7d47fe9dafdfabfed0cb40a
arun-94 <arunswaminathan94@gmail.com>
2015-05-30 02:29:59 +0530

    ProcessChat

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ function init() {
 	Parse.initialize("5CacqV3GaCWICl8LV19LXZ2G2JbzPmjlupfDKIte", "F5kE5mcmOy2wKS8qfRcPrRICSKdh7f3tjU8V2Qod");
 	console.log("Parse Initialized");
 
-	GetAccountDetails();
+	ProcessChat();
 }
 
 function MiniStatement() {
@@ -29,6 +29,19 @@ function GetBalance() {
 
 function GetAccountDetails() {
 	Parse.Cloud.run('GetAccountDetails', {}, {
+		success: function (result) {
+			console.log(result);
+		},
+		error: function (error) {
+			console.log(error);
+		}
+	});
+}
+
+function ProcessChat() {
+	Parse.Cloud.run('ProcessChat', {
+		inputString: "Give my balance"
+	}, {
 		success: function (result) {
 			console.log(result);
 		},
